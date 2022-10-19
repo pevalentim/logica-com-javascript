fc9076ee004eb48d98c274bf6fe08df3cb4159a4
valentim <pedamastim@gmail.com>
2022-10-19 19:41:06 -0300

    corrigindo

--- ex02/script.js
+++ ex02/script.js
@@ -1,7 +1,7 @@
 document.querySelector("button").addEventListener("click", function(){
-    let n1 = parseInt(prompt("Insira primeiro número"));
-    let n2 = parseInt(prompt("Agora, insira o segundo!"));
+    let n1 = parseFloat(prompt("Insira primeiro número"));
+    let n2 = parseFloat(prompt("Agora, insira o segundo!"));
     const soma = n1 + n2;
-    alert("A soma desses dois números é " + soma + ".");
+    alert("A soma desses dois números é " + soma);
 }
 )
