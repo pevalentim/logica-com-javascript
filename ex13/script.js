28007c4ce282a80ced54d1819f696a1e8566aca9
valentim <pedamastim@gmail.com>
2022-10-24 19:55:31 -0300

    ex13

---
+++ ex13/script.js
@@ -0,0 +1,12 @@
+document.querySelector("#btn").addEventListener("click", tabuada)
+
+function tabuada(){
+  const num = parseFloat(document.querySelector("#num").value);
+  let result = document.querySelector("#container");
+  let i = 1;
+  
+  while (i <= 100) {
+    result.innerHTML = (num + " x " + i + " = " + (num * i) + "</br>");
+    i++;
+  }
+}
