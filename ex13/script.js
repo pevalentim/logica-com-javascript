634d84bf4ddf74ca22410dc99373f3a311c19445
valentim <pedamastim@gmail.com>
2022-12-05 14:54:33 -0300

    add hr
    agora a cada 10 adiciona uma linha

--- ex13/script.js
+++ ex13/script.js
@@ -1,4 +1,4 @@
-document.querySelector("#btn").addEventListener("click", tabuada)
+document.querySelector("#btn").addEventListener("click", tabuada);
 
 function tabuada(){
   const num = parseFloat(document.querySelector("#num").value);
@@ -7,6 +7,11 @@ function tabuada(){
   
   while (i <= 100) {
     result.innerHTML += (num + " x " + i + " = " + (num * i) + "</br>");
+
+    if((i % 10) === 0){
+      result.innerHTML += "<hr>";
+    }
+
     i++;
   }
 }
