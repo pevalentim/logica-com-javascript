21d6015f34b0b15608727ae3afd3a4ee615d39c2
valentim <pedamastim@gmail.com>
2022-12-14 14:12:58 -0300

    add comment

--- ex14/script.js
+++ ex14/script.js
@@ -1,15 +1,16 @@
 document.querySelector("#btn").addEventListener("click", calc);
 
 function calc(){
-  let result = document.querySelector("#container");
-  let y = 1004;
+  let result = document.querySelector("#container"); // resultado vai aparecer aqui
+  let y = 1004; // ano inicial
   
   while (y <= 2024) {
-    if(((y % 100) > 0) || ((y % 400) === 0)){
+    if(((y % 100) > 0) || ((y % 400) === 0)){ // critério para saber se o ano é bissexto
     result.innerHTML += y + "<br>" ;
   } else {
     result.innerHTML += "" ;
   }
   y += 4;
   }
+
 };
